63efb662d9dfecad3d167326164a65a59e63f96f
jesse zenon <jesse.zenon@gmail.com>
2022-04-19 15:36:39 -0500

    added keydown event

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const randomURL =
-  "https://api.spoonacular.com/recipes/random?apiKey=5cfff0b79de64c61bbe518c3a5c52cb6";
+  "https://api.spoonacular.com/recipes/random?apiKey=44e9f8a09607483aa7fabed76a891bdb";
 
 const wholeURL =
-  "https://api.spoonacular.com/food/menuItems/search?apiKey=5cfff0b79de64c61bbe518c3a5c52cb6";
+  "https://api.spoonacular.com/recipes/complexSearch?apiKey=5cfff0b79de64c61bbe518c3a5c52cb6";
 fetch(randomURL)
   .then((response) => response.json())
-  .then(console.log);
+  .then((recipes) => getTheRecipe(recipes));
 
-function stuff() {
-  return "hello world";
+const recipePic = document.getElementById("list");
+
+function getTheRecipe(recipes) {
+  let toppings = recipes.recipes[0].extendedIngredients;
+
+  const showIngredients = document.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowRight") {
+      toppings.forEach((topping) => {
+        keyIngredients(topping);
+      });
+    }
+  });
 }
 
-let willThisWork = "I want to know if this will work";
+function keyIngredients(e) {
+  li = document.createElement("li");
+  li.textContent = e.name;
+  recipePic.append(li);
+}
