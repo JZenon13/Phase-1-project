b5fe6f7500a9ad341ff9a0edc3a926e9d9ab60b2
jesse zenon <jesse.zenon@gmail.com>
2022-04-20 08:21:18 -0500

    js,html, and css updates

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const randomURL =
   "https://api.spoonacular.com/recipes/random?apiKey=44e9f8a09607483aa7fabed76a891bdb";
 
-const wholeURL =
-  "https://api.spoonacular.com/recipes/complexSearch?apiKey=5cfff0b79de64c61bbe518c3a5c52cb6";
+// const wholeURL =
+//   "https://api.spoonacular.com/recipes/complexSearch?apiKey=5cfff0b79de64c61bbe518c3a5c52cb6";
 fetch(randomURL)
   .then((response) => response.json())
   .then((recipes) => getTheRecipe(recipes));
@@ -11,6 +11,11 @@ const recipePic = document.getElementById("list");
 
 function getTheRecipe(recipes) {
   let toppings = recipes.recipes[0].extendedIngredients;
+
+  foodImgSpot = document.getElementById("title-area");
+  foodImg = document.createElement("img");
+  foodImg.src = recipes.recipes[0].image;
+  foodImgSpot.append(foodImg);
 
   const showIngredients = document.addEventListener("keydown", (e) => {
     if (e.key === "ArrowRight") {
@@ -26,3 +31,21 @@ function keyIngredients(e) {
   li.textContent = e.name;
   recipePic.append(li);
 }
+
+const form = document.querySelector("form");
+form.addEventListener("submit", handleSubmit);
+function handleSubmit(e) {
+  e.preventDefault();
+  console.log(e);
+
+  let comment = document.getElementById("new-comment");
+  const submit = document.getElementById("comment-display");
+  comment = e.target[0].value;
+  submit.innerText = "";
+  submit.append(comment);
+  form.reset();
+}
+const button = document.getElementById("random");
+button.addEventListener("click", () => {
+  window.location.reload();
+});
